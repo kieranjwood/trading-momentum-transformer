089eca4b05adcbd347dda1bcf7a0fb286a28783b
Kieran Wood <kieran.wood@eng.ox.ac.uk>
2022-01-23 21:35:36 +0000

    corrected reference cexperiment

--- examples/run_classical_strategies.py
+++ examples/run_classical_strategies.py
@@ -3,7 +3,7 @@ from mom_trans.backtest import run_classical_methods
 
 INTERVALS = [(1990, y, y + 1) for y in range(2016, 2022)]
 
-REFERENCE_EXPERIMENT = "experiment_quandl_100assets_classical"
+REFERENCE_EXPERIMENT = "experiment_quandl_100assets_tft_cpnone_len252_notime_div_v1"
 
 features_file_path = os.path.join(
     "data",
